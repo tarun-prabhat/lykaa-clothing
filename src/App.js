f8288c439297da995fffad2858fb01f28f642294
tarun-prabhat <tarunprabhat1@gmail.com>
2021-02-23 03:21:47 +0530

    updated menu-items with withrouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage';
 import './App.css';
+
+const HatsPage =() => (<div>
+  <h1>
+    HatsPage
+  </h1>
+</div>
+)
+  
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+      </Switch>
+      
     </div>
   );
 }
